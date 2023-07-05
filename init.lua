2b685644494d8b8529dee87ff67d39b80a72c60d
Sokhuong Uon <sokhuong.usk@gmail.com>
2023-07-05 13:43:23 +0700

    refactor: remove map for comment

--- init.lua
+++ init.lua
@@ -2,7 +2,6 @@ require("plugins")
 local vscode = require("vscode")
 
 local whichkey = vscode.whichkey
-local comment = vscode.comment
 local refactor = vscode.refactor
 local fold = vscode.fold
 local git = vscode.git
@@ -39,92 +38,89 @@ if vim.g.vscode then
   -- whichkey
   vim.keymap.set({ 'n', 'v' }, "<leader>", whichkey.show)
 
-  -- comment
-  vim.keymap.set({ 'n', 'v' }, "<leader>/", comment.selected)
-
   -- orgainze import
-  vim.keymap.set({ 'n' }, "<leader>i", buffer.organizeImport)
+  vim.keymap.set('n', "<leader>i", buffer.organizeImport)
 
   -- show VSCode Editor Command ( Ctrl+Shift+P )
-  vim.keymap.set({ 'n', 'v' }, "<leader> ", workbench.showCommands)
+  vim.keymap.set('n', "<leader> ", workbench.showCommands)
 
   vim.keymap.set({ 'n', 'v' }, "H", workbench.previousEditor)
   vim.keymap.set({ 'n', 'v' }, "L", workbench.nextEditor)
 
   -- go to
-  vim.keymap.set({ 'n' }, "gn", gitGoto.nextChange, { noremap = true, silent = true })
-  vim.keymap.set({ 'n' }, "gN", gitGoto.showNextChange, { noremap = true, silent = true })
-  vim.keymap.set({ 'n' }, "gp", gitGoto.previousChange, { noremap = true, silent = true })
-  vim.keymap.set({ 'n' }, "gP", gitGoto.showPreviousChange, { noremap = true, silent = true })
+  vim.keymap.set('n', "gn", gitGoto.nextChange, { noremap = true, silent = true })
+  vim.keymap.set('n', "gN", gitGoto.showNextChange, { noremap = true, silent = true })
+  vim.keymap.set('n', "gp", gitGoto.previousChange, { noremap = true, silent = true })
+  vim.keymap.set('n', "gP", gitGoto.showPreviousChange, { noremap = true, silent = true })
 
   -- go to fold
-  vim.keymap.set({ 'n' }, "gf", fold.gotoParentFold, { noremap = true, silent = true })
+  vim.keymap.set('n', "gf", fold.gotoParentFold, { noremap = true, silent = true })
 
   -- problem
-  vim.keymap.set({ 'n' }, "<leader>pl", problem.list)
-  vim.keymap.set({ 'n' }, "<leader>pn", problem.next)
-  vim.keymap.set({ 'n' }, "<leader>pp", problem.previous)
+  vim.keymap.set('n', "<leader>pl", problem.list)
+  vim.keymap.set('n', "<leader>pn", problem.next)
+  vim.keymap.set('n', "<leader>pp", problem.previous)
 
   -- git
-  vim.keymap.set({ 'n' }, "<leader>gb", git.switch)
-  vim.keymap.set({ 'n' }, "<leader>gi", git.init)
-  vim.keymap.set({ 'n' }, "<leader>gd", git.deleteBranch)
-  vim.keymap.set({ 'n' }, "<leader>gf", git.fetch)
-  vim.keymap.set({ 'n' }, "<leader>gs", git.status)
-  vim.keymap.set({ 'n' }, "<leader>gp", git.pull)
-  vim.keymap.set({ 'n' }, "<leader>gg", git.graph)
-  vim.keymap.set({ 'n' }, "gr", gitGoto.revertChange, { noremap = true, silent = true })
+  vim.keymap.set('n', "<leader>gb", git.switch)
+  vim.keymap.set('n', "<leader>gi", git.init)
+  vim.keymap.set('n', "<leader>gd", git.deleteBranch)
+  vim.keymap.set('n', "<leader>gf", git.fetch)
+  vim.keymap.set('n', "<leader>gs", git.status)
+  vim.keymap.set('n', "<leader>gp", git.pull)
+  vim.keymap.set('n', "<leader>gg", git.graph)
+  vim.keymap.set('n', "gr", gitGoto.revertChange, { noremap = true, silent = true })
 
   -- buffer
-  vim.keymap.set({ 'n' }, "<leader>c", buffer.close)
-  vim.keymap.set({ 'n' }, "<leader>bc", buffer.close)
-  vim.keymap.set({ 'n' }, "<leader>k", buffer.closeOther)
-  vim.keymap.set({ 'n' }, "<leader>bk", buffer.closeOther)
-  vim.keymap.set({ 'n' }, "<leader>w", buffer.save)
-  vim.keymap.set({ 'n' }, "<leader>wa", buffer.saveAll)
-  vim.keymap.set({ 'n' }, "<leader>bf", buffer.format)
-  vim.keymap.set({ 'n' }, "<leader>bn", buffer.new)
-  vim.keymap.set({ 'n' }, "<leader>bt", buffer.showInExplorer)
-  vim.keymap.set({ 'n' }, "<leader>br", buffer.rename)
+  vim.keymap.set('n', "<leader>c", buffer.close)
+  vim.keymap.set('n', "<leader>bc", buffer.close)
+  vim.keymap.set('n', "<leader>k", buffer.closeOther)
+  vim.keymap.set('n', "<leader>bk", buffer.closeOther)
+  vim.keymap.set('n', "<leader>w", buffer.save)
+  vim.keymap.set('n', "<leader>wa", buffer.saveAll)
+  vim.keymap.set('n', "<leader>bf", buffer.format)
+  vim.keymap.set('n', "<leader>bn", buffer.new)
+  vim.keymap.set('n', "<leader>bt", buffer.showInExplorer)
+  vim.keymap.set('n', "<leader>br", buffer.rename)
 
   -- toggle
-  vim.keymap.set({ 'n' }, "<leader>ta", toggle.activityBar)
-  vim.keymap.set({ 'n' }, "<leader>tz", toggle.zenMode)
-  vim.keymap.set({ 'n' }, "<leader>ts", toggle.sideBarVisibility)
-  vim.keymap.set({ 'n' }, "<leader>tt", toggle.theme)
-  vim.keymap.set({ 'n' }, "<leader>tl", toggle.breadcrumbs)
+  vim.keymap.set('n', "<leader>ta", toggle.activityBar)
+  vim.keymap.set('n', "<leader>tz", toggle.zenMode)
+  vim.keymap.set('n', "<leader>ts", toggle.sideBarVisibility)
+  vim.keymap.set('n', "<leader>tt", toggle.theme)
+  vim.keymap.set('n', "<leader>tl", toggle.breadcrumbs)
 
   -- refactor
-  vim.keymap.set({ 'n' }, "<leader>r", refactor.showMenu)
-  vim.keymap.set({ 'n' }, "<leader>rr", refactor.rename)
+  vim.keymap.set('n', "<leader>r", refactor.showMenu)
+  vim.keymap.set('n', "<leader>rr", refactor.rename)
   -- refactor html
   vim.keymap.set({ 'n', 'v' }, "<leader>rw", refactor.html.wrap)
   vim.keymap.set({ 'n', 'v' }, "<leader>rd", refactor.html.delete)
   vim.keymap.set({ 'n', 'v' }, "<leader>ru", refactor.html.update)
 
   -- search
-  vim.keymap.set({ 'n' }, "<leader>sr", search.reference)
-  vim.keymap.set({ 'n' }, "<leader>ss", search.symbols)
-  vim.keymap.set({ 'n' }, "<leader>sR", search.referenceInSideBar)
-  vim.keymap.set({ 'n' }, "<leader>sp", search.project)
-  vim.keymap.set({ 'n' }, "<leader>st", search.text)
-  vim.keymap.set({ 'n' }, "<leader>sf", search.file)
+  vim.keymap.set('n', "<leader>sr", search.reference)
+  vim.keymap.set('n', "<leader>ss", search.symbols)
+  vim.keymap.set('n', "<leader>sR", search.referenceInSideBar)
+  vim.keymap.set('n', "<leader>sp", search.project)
+  vim.keymap.set('n', "<leader>st", search.text)
+  vim.keymap.set('n', "<leader>sf", search.file)
 
   -- selection
-  vim.keymap.set({ 'v' }, "<leader>s", function()
+  vim.keymap.set('v', "<leader>s", function()
     vim.fn.VSCodeNotifyRange("setSelection", vim.fn.line("v"), vim.fn.line("."), 1)
   end, { noremap = true, silent = true })
 
   -- copy
-  vim.keymap.set({ 'v' }, "<leader>y", function()
+  vim.keymap.set('v', "<leader>y", function()
     vim.fn.VSCodeNotifyRange("setSelection", vim.fn.line("v"), vim.fn.line("."), 1)
     vim.fn.VSCodeNotify("editor.action.clipboardCopyAction")
   end, { noremap = true, silent = true })
 
   --folding
-  vim.keymap.set({ 'n' }, "zr", fold.openAll)
-  vim.keymap.set({ 'n' }, "zm", fold.all)
-  vim.keymap.set({ 'n' }, "za", fold.toggle)
+  vim.keymap.set('n', "zr", fold.openAll)
+  vim.keymap.set('n', "zm", fold.all)
+  vim.keymap.set('n', "za", fold.toggle)
   --#endregion keymap
 else
   -- ordinary Neovim
