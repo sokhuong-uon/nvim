6f14ba8c59f5556e56667f22d362abbddc303677
Sokhuong Uon <sokhuong.usk@gmail.com>
2024-08-10 19:49:11 +0700

    fix: disable backdrop

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -3,14 +3,14 @@ return {
     "folke/flash.nvim",
     event = "VeryLazy",
     ---@type Flash.Config
-    opts = {},
-    -- stylua: ignore
+    opts = {
+      highlight = {
+        backdrop = false,
+      },
+    },
     keys = {
       { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end,       desc = "Flash" },
       { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
-      -- { "r",     mode = "o",               function() require("flash").remote() end,            desc = "Remote Flash" },
-      -- { "R",     mode = { "o", "x" },      function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
-      -- { "<c-s>", mode = { "c" },           function() require("flash").toggle() end,            desc = "Toggle Flash Search" },
     },
   },
   {
@@ -22,11 +22,5 @@ return {
         -- Configuration here, or leave empty to use defaults
       })
     end
-  },
-  {
-    "folke/tokyonight.nvim",
-    lazy = false,
-    priority = 1000,
-    opts = {},
   }
 }
